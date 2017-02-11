3411071cb5f72d3b586ab0eb129fa6217dd03ae4
BirdZhang <0312birdzhang@gmail.com>
2017-02-11 16:07:02 +0800

    init commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(function() {
+	/**
+	 * Back key event handler
+	 */
+	window.addEventListener('tizenhwkey', function(ev) {
+		if (ev.keyName === "back") {
+			var current = new Date();
+			var year = current.getFullYear();
+			var month = current.getMonth()+1;
+			var day = current.getDate();
+			month = month<10 ? '0'+month : month;
+			day = day<10 ? '0'+day : day;
+			var current_day = year+'-'+month+'-'+day;
+			var selected_day = $("#today_num").html();
+			console.log("current_day:"+current_day);
+			console.log("selected_day:"+selected_day);
+			if (current_day == selected_day) {
+				//popup
+				tau.openPopup("#quit_popup");
+//				try {
+//					tizen.application.getCurrentApplication().exit();
+//				} catch (ignore) {
+//					console.log(ignore);
+//				}
+			} else {
+				console.log("need go back today");
+				$('.today').trigger("tap");
+			}
+		}
+	});
+}());
+
